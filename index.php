5e204aab71a9975cb3a4a2cae002732f283c69f8
Rican7 <rican7@gmail.com>
2012-01-11 22:52:33 +0000

    Changed the Klein function to catch all url's (for now). And changed the PSUTemplate calls over to my new, custom Mobile smarty template class.
    git-svn-id: svn://svn.plymouth.edu/psu/webapp/branches/mobile-jquery@10609 764ba614-8a4f-45ce-84a4-4093f4c8669b

--- index.php
+++ index.php
@@ -3,11 +3,14 @@
 require_once 'autoload.php';
 PSU::session_start(); // force ssl + start a session
 
-$GLOBALS['BASE_URL'] = '/webapp/mobile/';
+$GLOBALS['BASE_URL'] = '/webapp/mobile';
 $GLOBALS['BASE_DIR'] = __DIR__;
 
 $GLOBALS['TITLE'] = 'PSU Mobile';
-$GLOBALS['TEMPLATES'] = $GLOBALS['BASE_DIR'] . '/templates';
+$GLOBALS['TEMPLATES'] = $GLOBALS['BASE_DIR'] . '/templates/';
+
+// Require my custom mobile smarty class
+require_once $GLOBALS['BASE_DIR'] . '/includes/MobileTemplate.class.php';
 
 require_once $GLOBALS['BASE_DIR'] . '/API.php';
 require_once 'klein/klein.php';
@@ -31,6 +34,7 @@ if( ! IDMObject::authZ('permission', 'mis') ) {
  * Make some objects available elsewhere.
  */
 respond( function( $request, $response, $app ) {
+	/*
 	// initialize the template
 	$app->tpl = new PSUTemplate;
 
@@ -39,4 +43,22 @@ respond( function( $request, $response, $app ) {
 
 	// assign user to template
 	$app->tpl->assign( 'user', $app->user );
+	 */
 });
+
+// Klein catch-all (should be more developed later)
+respond( '[*]', function( $request, $response, $app ) {
+	// Initialize the PSU smarty templating
+	$app->tpl = new MobileTemplate;
+
+	// Display the default template (index)
+	$app->tpl->display( 'index.tpl', false );
+});
+
+$app_routes = array();
+
+foreach( $app_routes as $base ) {
+	with( "/{$base}", $GLOBALS['BASE_DIR'] . "/routes/{$base}.php" );
+}//end foreach
+
+dispatch( $_SERVER['PATH_INFO'] );
