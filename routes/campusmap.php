ef365d10c96a2336d4eed417b46773ddda0d690e
Rican7 <rican7@gmail.com>
2012-01-13 22:22:07 +0000

    Added jquery-ui-map to the template
    git-svn-id: svn://svn.plymouth.edu/psu/webapp/branches/mobile-jquery@10646 764ba614-8a4f-45ce-84a4-4093f4c8669b

--- routes/campusmap.php
+++ routes/campusmap.php
@@ -6,7 +6,13 @@ respond( '[/?]', function( $request, $response, $app ){
 	$pre_jq_js = array(
 		'http://maps.google.com/maps/api/js?sensor=true',
 	);
+	
+	// Create an array to be used to load post jquery mobile javascript
+	$post_jqm_js = array(
+		'/webapp/mobile/js/jquery.ui.map.full.min.js',
+	);
 
 	$app->tpl->assign( 'pre_jq_js', $pre_jq_js );
+	$app->tpl->assign( 'post_jqm_js', $post_jqm_js );
 	$app->tpl->display( 'campusmap.tpl' );
 });
