f055e145cd66eb1f423baa6bcf9db676395e5c58
Rican7 <rican7@gmail.com>
2012-01-13 17:54:27 +0000

    Able to access the static classes from my routes. :D
    git-svn-id: svn://svn.plymouth.edu/psu/webapp/branches/mobile-jquery@10630 764ba614-8a4f-45ce-84a4-4093f4c8669b

--- routes/newsfeed.php
+++ routes/newsfeed.php
@@ -2,5 +2,8 @@
 
 // Generic response (don't force the trailing slash: this should catch any accidental laziness)
 respond( '[/?]', function( $request, $response, $app ){
+	// Grab the news
+	Mobile::newsfeed();
+
 	$app->tpl->display( 'newsfeed.tpl' );
 });
