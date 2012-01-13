57e6c22e2571aaf80c2cfcc176792f1af3622e12
Rican7 <rican7@gmail.com>
2012-01-13 20:31:24 +0000

    Modified the route to decode the json data from the newsfeeds and assign the data as an array into the template
    git-svn-id: svn://svn.plymouth.edu/psu/webapp/branches/mobile-jquery@10635 764ba614-8a4f-45ce-84a4-4093f4c8669b

--- routes/newsfeed.php
+++ routes/newsfeed.php
@@ -3,7 +3,11 @@
 // Generic response (don't force the trailing slash: this should catch any accidental laziness)
 respond( '[/?]', function( $request, $response, $app ){
 	// Grab the news
-	Mobile::newsfeed();
+	$feed_json = Mobile::newsfeed();
 
+	// Decode the JSON into a more PHP usable array
+	$feed_data = json_decode( $feed_json );
+
+	$app->tpl->assign( 'feed_data', $feed_data );
 	$app->tpl->display( 'newsfeed.tpl' );
 });
