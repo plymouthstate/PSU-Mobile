ade45c158141a3d928043b5afe91bc23c558d67a
Rican7 <rican7@gmail.com>
2012-01-13 14:58:43 +0000

    Created (resurrected) the Feedback class, and added the caller into the main class. Also, renamed the Newsfeeds class to better differentiate and represent the class.
    git-svn-id: svn://svn.plymouth.edu/psu/webapp/branches/mobile-jquery@10626 764ba614-8a4f-45ce-84a4-4093f4c8669b

--- includes/Mobile/Mobile.php
+++ includes/Mobile/Mobile.php
@@ -4,4 +4,45 @@ namespace Mobile;
 
 class Mobile {
 
+	/**
+	 * Newsfeed aggregator.
+	 * Grabs different feeds, combines them, and converts them to JSON
+	 * format for easy AJAX parsing
+	 */
+	public function newsfeed() {
+		// Get the data from each source
+		$feed_data['twitter'] = MobileNewsfeeds::twitter();
+		$feed_data['facebook'] = MobileNewsfeeds::facebook();
+		$feed_data['rss'] = MobileNewsfeeds::rss();
+
+		// Aggregate the feeds into one feed for JSON response
+		$agg_feed_data = MobileNewsfeeds::aggregate($feed_data);
+
+		echo json_encode($agg_feed_data);
+	} // End newsfeed
+
+	/**
+	 * Feedback Mailer
+	 * Takes in JSON formatted data
+	 * Validates it like a form
+	 * Mails the ITS Helpdesk
+	 * And then responds with JSON
+	 */
+	public function feedback() {
+		// Localize and decode POST data
+		$postData = json_decode(stripslashes($_POST['postData']), true);
+		$responseData = array();
+
+		// Initialize the response data
+		$responseData = array();
+
+		// If data is valid
+		if(MobileFeedback::validate_data($postData, $responseData)) {
+			// Submit the feedback
+			MobileFeedback::submit_feedback($postData, $responseData);
+		}
+
+		echo json_encode($responseData);
+	} // End feedback
+
 } // End class Mobile
