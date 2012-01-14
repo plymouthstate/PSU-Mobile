4255dd0a7cb72888af1e3f6f2ad455865e24cd21
Rican7 <rican7@gmail.com>
2012-01-14 00:10:59 +0000

    Completely restructured the global js. Everything's handled with page-specific event handlers and works properly on multiple pages.
    git-svn-id: svn://svn.plymouth.edu/psu/webapp/branches/mobile-jquery@10650 764ba614-8a4f-45ce-84a4-4093f4c8669b

--- js/global-logic.js
+++ js/global-logic.js
@@ -12,6 +12,36 @@
 		}
 	};
 	*/
+
+	// Get the device's OS and add it as a class to the HTML tag
+	// To be used for OS-specific styling (trying to stay more true to the native styling)
+	$.fn.deviceOS = function() {
+		var deviceOS = '';
+
+		// Get the data
+		var devicePlatform = navigator.userAgent.match(/Android/i)
+			|| navigator.userAgent.match(/iPod/i)
+			|| navigator.userAgent.match(/iPad/i)
+			|| navigator.userAgent.match(/iPhone/i)
+			|| navigator.userAgent.match(/webOS/i)
+			|| 'Other';
+
+		if (devicePlatform == 'iPod' || devicePlatform == 'iPad' || devicePlatform == 'iPhone') {
+			deviceOS = 'ios';
+		}
+		else if (devicePlatform == 'Android') {
+			deviceOS = 'android';
+		}
+		else {
+			deviceOS = devicePlatform.toLowerCase();
+		}
+
+		// Add the deviceOS as a CSS class to the HTML tag
+		$('html').addClass(deviceOS);
+
+		return deviceOS;
+	};
+
 })(jQuery);
 
 // Bind events to be triggered BEFORE jQuery Mobile loads/executes
@@ -20,14 +50,28 @@ $(document).bind('mobileinit', function() {
 	//$.mobile.touchOverflowEnabled = true;
 });
 
-// Bind events to be triggered on page initialization
-$(document).bind('pageinit', function() {
-	// Detect orientation change
-	// Bind the event to the window
+// Bind generic events to be triggered on EVERY page initialization
+$(document).live('pageinit', function() {
+	// Function to change the class of the HTML tag based on the orientation of the device
+	function changeOrientationClass(orientation) {
+		// Add the orientation as a CSS class to the HTML tag
+		$('html').addClass(orientation);
+	}
+
+	// Functions to run on orientation change
 	$(window).bind('orientationchange', function(event){
-		changeOrientation(event.orientation);
+		changeOrientationClass(event.orientation);
 	});
 
+	// Functions to run on page-load
+	(function() {
+		$(window).trigger('orientationchange');
+		$().deviceOS();
+	})();
+});
+
+// Bind generic events to be triggered on the DASHBOARD page initialization
+$('#page-dashboard').live('pageinit', function() {
 	// Detect and mark the "middle" elements of the dashboard
 	function detectMiddleElements() {
 		// Grab the width of the entire dashboard
@@ -56,39 +100,10 @@ $(document).bind('pageinit', function() {
 		$('nav#dashboard ul#dashboard-mapps li:nth-child(' + everyNthChild +')').addClass('dash-middle-element');
 	}
 
-	// Get the device's OS and add it as a class to the HTML tag
-	// To be used for OS-specific styling (trying to stay more true to the native styling)
-	function deviceOS() {
-		var deviceOS = '';
-
-		// Get the data
-		var devicePlatform = navigator.userAgent.match(/Android/i)
-			|| navigator.userAgent.match(/iPod/i)
-			|| navigator.userAgent.match(/iPad/i)
-			|| navigator.userAgent.match(/iPhone/i)
-			|| navigator.userAgent.match(/webOS/i)
-			|| 'Other';
-
-		if (devicePlatform == 'iPod' || devicePlatform == 'iPad' || devicePlatform == 'iPhone') {
-			deviceOS = 'ios';
-		}
-		else if (devicePlatform == 'Android') {
-			deviceOS = 'android';
-		}
-		else {
-			deviceOS = devicePlatform.toLowerCase();
-		}
-
-		// Add the deviceOS as a CSS class to the HTML tag
-		$('html').addClass(deviceOS);
-
-		return deviceOS;
-	}
-
 	// Detect device and meta info and display it on the page
 	function deviceInfo() {
 		// Get the data
-		var devicePlatform = deviceOS();
+		var devicePlatform = $().deviceOS();
 		var displayDensity = window.devicePixelRatio;
 
 		// Display it by adding it to the mobile-info span
@@ -97,6 +112,7 @@ $(document).bind('pageinit', function() {
 
 	}
 
+	// Make the info button footer clickable
 	$('.info-button').click(function() {
 		$('#hidden-info-div').animate({ height: 'toggle', leaveTransforms: true, useTranslate3d: true}, 800, 'easeOutExpo', function() {
 			// Fix window height bugs by triggering an updatelayout and resize (repaint, please)
@@ -104,21 +120,35 @@ $(document).bind('pageinit', function() {
 			$(this).trigger('updatelayout');
 		});
 	});
-	
-	// Functions to run on orientation change
-	function changeOrientation(orientation) {
-		// Add the orientation as a CSS class to the HTML tag
-		$('html').addClass(orientation);
 
-		// Fire off some functions
+	// Functions to run on orientation change
+	$(window).bind('orientationchange', function(event){
 		detectMiddleElements();
-	}
+	});
 
 	// Functions to run on page-load
 	(function() {
-		$(window).trigger('orientationchange');
 		detectMiddleElements();
-		deviceOS();
 		deviceInfo();
 	})();
+});
+
+// Bind generic events to be triggered on the CAMPUS MAP page initialization
+$('#page-campusmap').live('pageinit', function() {
+	// Create a Google Map
+	var yourStartLatLng = new google.maps.LatLng(43.758976, -71.688709);
+	var zoomLevel = 15;
+	var gmapObject = {'center': yourStartLatLng, 'zoom': zoomLevel};
+
+	// Refresh it on show
+	$('#page-campusmap').live("pageshow", function() {
+		// Create the map
+		$('div#campus-google-map').gmap('refresh');
+	});
+
+	// Fire on page-create
+	$('#page-campusmap').live("pagecreate", function() {
+		// Create the map
+		$('div#campus-google-map').gmap( gmapObject );
+	});
 });
