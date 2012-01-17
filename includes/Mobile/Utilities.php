2267a3c3950a159f255b79f16aec11e681a84f26
Rican7 <rican7@gmail.com>
2012-01-17 15:25:00 +0000

    Added a function to properly clean advanced unicode characters
    git-svn-id: svn://svn.plymouth.edu/psu/webapp/branches/mobile-jquery@10662 764ba614-8a4f-45ce-84a4-4093f4c8669b

--- includes/Mobile/Utilities.php
+++ includes/Mobile/Utilities.php
@@ -31,4 +31,26 @@ class Utilities {
 		return $strDate;
 	} // End datetime
 
+	/**
+	 * Function to strip out strange unicode characters, such as smart-quotes
+	 * SOURCE: http://stackoverflow.com/a/4583465
+	 */
+	public static function html_all_entities($str){
+		$res = '';
+		$strlen = strlen($str);
+		for($i=0; $i<$strlen; $i++){
+			$byte = ord($str[$i]);
+			if($byte < 128) // 1-byte char
+				$res .= $str[$i];
+			elseif($byte < 192); // invalid utf8
+			elseif($byte < 224) // 2-byte char
+				$res .= '&#'.((63&$byte)*64 + (63&ord($str[++$i]))).';';
+			elseif($byte < 240) // 3-byte char
+				$res .= '&#'.((15&$byte)*4096 + (63&ord($str[++$i]))*64 + (63&ord($str[++$i]))).';';
+			elseif($byte < 248) // 4-byte char
+				$res .= '&#'.((15&$byte)*262144 + (63&ord($str[++$i]))*4096 + (63&ord($str[++$i]))*64 + (63&ord($str[++$i]))).';';
+		}
+		return $res;
+	} // End html_all_entities
+
 } // End class Utilities
