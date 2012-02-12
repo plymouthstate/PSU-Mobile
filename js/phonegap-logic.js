10cab4d165356e4503aeaf2b4f35999d25956bcc
Trevor Suarez <rican7@gmail.com>
2012-02-12 00:20:44 -0500

    Noob mistake. I wasn't "showing" the right element.

--- js/phonegap-logic.js
+++ js/phonegap-logic.js
@@ -4,7 +4,8 @@ document.addEventListener('deviceready', function () { // Don't use a jQuery eve
 	// Let the log know that PhoneGap is working! :)
 	console.log('DEVICEREADY event fired. PhoneGap has been initialized');
 
-	// Add the PhoneGap version to the info panel
+	// Add the PhoneGap version to the info panel and show it
 	var $infoPanel = $('.info-panel .app-frameworks');
-	$infoPanel.find('#phonegap span').text(device.phonegap).show(0);
+	$infoPanel.find('#phonegap span').text(device.phonegap);
+	$infoPanel.find('#phonegap').show(0);
 });
