8073cf0ed5c60d0cd3492f74f5b1de0147d3b08e
Trevor Suarez <rican7@gmail.com>
2012-02-12 00:15:19 -0500

    Created a new phonegap-logic file (possibly temporary... depending on how I structure the app later). The new phonegap-logic file will hold all the logic that is specific to the PhoneGap API. Which is only available when properly loaded with the client app. Added the script tag to load it in the footer. :)

---
+++ js/phonegap-logic.js
@@ -0,0 +1,10 @@
+// Let's listen for when PhoneGap has correctly loaded
+// THEN we'll run our PhoneGap dependent code
+document.addEventListener('deviceready', function () { // Don't use a jQuery event listener here. PhoneGap will shit itself.
+	// Let the log know that PhoneGap is working! :)
+	console.log('DEVICEREADY event fired. PhoneGap has been initialized');
+
+	// Add the PhoneGap version to the info panel
+	var $infoPanel = $('.info-panel .app-frameworks');
+	$infoPanel.find('#phonegap span').text(device.phonegap).show(0);
+});
