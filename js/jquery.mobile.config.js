f0ae341eec7ee038bb30feebc129542a9fb65e16
Trevor Suarez <rican7@gmail.com>
2012-04-18 14:04:33 -0400

    Set the default page transition BACK to the old default: slide.

--- js/jquery.mobile.config.js
+++ js/jquery.mobile.config.js
@@ -1,7 +1,13 @@
 // Bind events to be triggered BEFORE jQuery Mobile loads/executes
 $(document).bind('mobileinit', function() {
+	/*
 	// Set jQuery mobile settings
+	*/
+
 	//$.mobile.addBackBtn = true;
 	$.mobile.page.prototype.options.backBtnText = 'back';
 	$.mobile.page.prototype.options.backBtnTheme = 'c';
+
+	// Set the default page transition to "slide"
+	$.mobile.defaultPageTransition = 'slide';
 });
