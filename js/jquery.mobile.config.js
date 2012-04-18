5e7d86e25649f33a78be1ef19f2bc78bd02d7d60
Trevor Suarez <rican7@gmail.com>
2012-04-18 14:01:29 -0400

    Removed old way of overriding the header/footer toggle, and updated to the new way. Also removed deprecated options from the jquery mobile config file.

--- js/jquery.mobile.config.js
+++ js/jquery.mobile.config.js
@@ -1,8 +1,6 @@
 // Bind events to be triggered BEFORE jQuery Mobile loads/executes
 $(document).bind('mobileinit', function() {
 	// Set jQuery mobile settings
-	//$.mobile.touchOverflowEnabled = true;
-	$.mobile.fixedToolbars.setTouchToggleEnabled(false);
 	//$.mobile.addBackBtn = true;
 	$.mobile.page.prototype.options.backBtnText = 'back';
 	$.mobile.page.prototype.options.backBtnTheme = 'c';
